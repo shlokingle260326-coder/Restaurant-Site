2f4bf67164e4cafcf0eccf2a7081b8f50216beaa
Shlok Ingle <shlokingle260326@gmail.com>
2026-01-15 16:33:12 +0530

    Delete Restaurant-Site directory

--- Restaurant-Site/assets/main.js
+++
@@ -1,6 +0,0 @@
-// Navbar shadow on scroll
-window.addEventListener("scroll", () => {
-    const nav = document.querySelector(".navbar");
-    nav.classList.toggle("shadow", window.scrollY > 50);
-});
-
